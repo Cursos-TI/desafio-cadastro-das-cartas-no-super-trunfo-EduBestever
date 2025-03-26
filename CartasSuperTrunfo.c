023c462593bb45ef1683e8915bc7f5d60e976a4b
EduBestever <esa28082004@gmail.com>
2025-03-26 17:12:13 +0000

    CartasSuperTrunfo1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,69 +1,42 @@
 #include<stdio.h>
-//FUNÇÕES DE ENTRADA//
-int main(){
+struct Carta {
+    char estado;
+    char cidade;
+    int populacao;
+    float area;
+    float pib;
+    int pontos;
+};
+struct Carta carta1, carta2;
 
-char estado1,estado2;
-char cidade1,cidade2;
-int populacao1,populacao2;
-float area1,area2;  
-float pib1,pib2;
-int pontos1,pontos2;
+//ENTRADA PARA ESCREVER CARTA1
 
-//OBS: ESQUECI DE USAR O \N. ISSO FEZ O CODIGO SE MISTURAR TODO NO PRINT!
-
-// CODIGOS PARA FAZER A CARTA//
-printf("CARTA 1 \n"); //print da carta 1
-
-printf("Digite o estado 1(A-H): \n");   //print do estado
-scanf(" %c",&estado1);//
-
-printf("Digite a cidade 1(1-4): \n");   //print da cidade
-scanf(" %c",&cidade1);
-
-printf("Digite a populacao 1: \n");  //print populacao
-scanf("%d",&populacao1);
-
-printf("Digite a area 1: \n"); // print da area 
-scanf("%f",&area1);
-
-printf("Digite o pib 1: \n"); // população do país
-scanf("%f",&pib1);
-
-printf("Digite os pontos 1: \n");
-scanf("%d",&pontos1);
-
-printf("CARTA 2\n");
-
-printf("Digite o estado 2(A-H): \n"); //print do estado2
-scanf(" %c",&estado2);//
-printf("Digite a cidade 2(1-4): \n");//print da cidade2
-scanf(" %c",&cidade2);
-printf("Digite a populacao 2: \n");//print populacao2
-scanf("%d",&populacao2);
-printf("Digite a area 2: \n"); // print da area2
-scanf("%f",&area2);
-printf("Digite o pib 2: \n"); // população do país2
-scanf("%f",&pib2);
-printf("Digite os pontos 2: \n");//pontos turisticos2
-scanf("%d",&pontos2);
-
-
-//PRINT DAS CARTAS 
-
-printf("CARTA 1\n");
-printf("Estado: %c0%c\n",estado1,cidade1);
-printf("Populacao:%d\n",populacao1); 
-printf("Area: %.1f\n",area1);
-printf("PIB: %.1f\n",pib1);
-printf("Pontos: %d\n",pontos1); 
-printf("\n");
-printf("CARTA 2\n");
-printf("Estado: %c0%c\n",estado2,cidade2);
-printf("Populacao: %d\n",populacao2);
-printf("Area: %.1f\n",area2);
-printf("PIB: %.1f\n",pib2);
-printf("Pontos: %d\n",pontos2);
-return 0;
+void entrada1(){
+    printf("Digite o estado da carta 1: ");
+    scanf("%c", &carta1.estado);
+    printf("Digite a cidade da carta 1: ");
+    scanf("%c", &carta1.cidade);
+    printf("Digite a populacao da carta 1: ");
+    scanf("%d", &carta1.populacao);
+    printf("Digite a area da carta 1: ");
+    scanf("%f", &carta1.area);
+    printf("Digite o pib da carta 1: ");
+    scanf("%f", &carta1.pib);
+    printf("Digite os pontos turisticos da carta 1: ");
+    scanf("%d", &carta1.pontos);
 }
-
- 
+//ENTRADA PARA ESCREVER CARTA2  
+void entrada2(){
+    printf("Digite o estado da carta 1: ");
+    scanf("%c", &carta2.estado);
+    printf("Digite a cidade da carta 1: ");
+    scanf("%c", &carta2.cidade);
+    printf("Digite a populacao da carta 1: ");
+    scanf("%d", &carta2.populacao);
+    printf("Digite a area da carta 1: ");
+    scanf("%f", &carta2.area);
+    printf("Digite o pib da carta 1: ");
+    scanf("%f", &carta2.pib);
+    printf("Digite os pontos turisticos da carta 1: ");
+    scanf("%d", &carta2.pontos);
+}
